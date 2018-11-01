37595952a43a553b486c896f09365ee921261b14
mikaup <mikaup@metropolia.fi>
2018-11-01 15:42:23 +0200

    message

---
+++ DOM-tehtavat-master/js/teht1.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('td')[0].innerHTML = 'Mika';
+document.querySelectorAll('td')[1].innerHTML = 'Uponen';
+document.querySelectorAll('td')[2].innerHTML = 'ei ole';
+
+
+const tokarivi = document.querySelectorAll('tr')[1];
+
+tokarivi.innerHTML += '<td>045 110 1920</td>';
+
+const lisays = document.querySelectorAll('tr')[0];
+
+lisays.innerHTML += '<th>Puh.</th>';
+
+document.querySelectorAll('td')[2].innerHTML = delete('td');
+document.querySelectorAll('tr')[2];
+elem.parentNode.removeChild(elem);
+
