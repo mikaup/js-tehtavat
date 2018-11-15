fa29c4a24c2ddb5d2e0ff903f0d6a089bf7aec0d
mikaup <mikaup@metropolia.fi>
2018-11-15 15:15:39 +0200

    message

--- AjaxSovellus/haku.js
+++ AjaxSovellus/haku.js
@@ -1,63 +1,56 @@
-const nappi = document.querySelector("#hakunappi");
+const nappi = document.querySelector('#hakunappi');
 nappi.addEventListener('click', function(evt) {
   let haku = arvo.value;
   console.log(haku);
-  console.log(' http://api.tvmaze.com/search/shows?q='+haku);
+  console.log(' http://api.tvmaze.com/search/shows?q=' + haku);
 
-
-
-
-  fetch(' http://api.tvmaze.com/search/shows?q='+haku).then(function(vastaus) {
-    return vastaus.json();
-  }).then(function(json) {
-    console.log(json);
-    nayta(json);
-  }).catch(function(error) {
-    console.log(error);
-  });
-
-
-
+  fetch(' http://api.tvmaze.com/search/shows?q=' + haku).
+      then(function(vastaus) {
+        return vastaus.json();
+      }).
+      then(function(json) {
+        console.log(json);
+        nayta(json);
+      }).
+      catch(function(error) {
+        console.log(error);
+      });
 
 });
 
-const nayta = (lista) =>
-{
-  for (let i=0; i < lista.length; i++){
+const nayta = (lista) => {
+  for (let i = 0; i < lista.length; i++) {
+    let v = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
+    
+    try {
+      v = lista[i].show.image.medium;
+    } catch (e) {
+      
+    }
+    
     let esine = document.createElement('div');
     let otsikko = document.createElement('h1');
-    let kuva =  document.createElement('img');
+    let kuva = document.createElement('img');
     let kuvaus = document.createElement('p');
-    let linkki = document.createElement('p');
+    let linkki = document.createElement('p1');
     let genre = document.createElement('h3');
 
-
     //nimi
-    let x =lista[i].show.name;
+    let x = lista[i].show.name;
     //linkki
-    let y =lista[i].show.url;
+    let y = lista[i].show.url;
     //summary
-    let z =lista[i].show.summary;
-   //kuva
-     let v = lista[i].show.image.medium;
+    let z = lista[i].show.summary;
+    //kuva
+
     //genre
-    let g =lista[i].show.genres;
+    let g = lista[i].show.genres;
 
-    try {
-       v = lista[i].show.image.medium;
-    }catch (error) {
-      console.error(error);
-      kuva.src = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
-    }
-
-
-
-      otsikko.innerHTML = x;
-      linkki.innerHTML = y;
-      kuvaus.innerHTML = z;
-      kuva.src = v;
-      genre.innerHTML = 'Genre:' +g;
-
+    otsikko.innerHTML = x;
+    linkki.innerHTML = y;
+    kuvaus.innerHTML = z;
+    kuva.src = v;
+    genre.innerHTML = 'Genre:' + g;
 
     tulosta.appendChild(esine);
     tulosta.appendChild(otsikko);
@@ -66,20 +59,13 @@ const nayta = (lista) =>
     tulosta.appendChild(genre);
     tulosta.appendChild(linkki);
 
-
-
   }
 };
 
-
-let arvo = document.querySelector("#hakuteksti");
-
-
+let arvo = document.querySelector('#hakuteksti');
 
 console.log('ready');
 const tulosta = document.querySelector('#tulosta');
-
-
 
 // https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
 // nayta(json);
@@ -103,3 +89,5 @@ const tulosta = document.querySelector('#tulosta');
 //       kuvaus.innerHTML = z;
 //
 //       }else { }
+
+// let v = lista[i].show.image.medium;
