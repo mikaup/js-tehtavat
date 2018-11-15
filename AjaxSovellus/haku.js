3fe90580040c58c84a7d031b6d604b7132005fe3
mikaup <mikaup@metropolia.fi>
2018-11-15 14:21:03 +0200

    message

--- AjaxSovellus/haku.js
+++ AjaxSovellus/haku.js
@@ -3,34 +3,103 @@ nappi.addEventListener('click', function(evt) {
   let haku = arvo.value;
   console.log(haku);
   console.log(' http://api.tvmaze.com/search/shows?q='+haku);
-console.log(nimi);
-console.log(tyyppi);
+
+
+
+
+  fetch(' http://api.tvmaze.com/search/shows?q='+haku).then(function(vastaus) {
+    return vastaus.json();
+  }).then(function(json) {
+    console.log(json);
+    nayta(json);
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+
+
+
 });
+
+const nayta = (lista) =>
+{
+  for (let i=0; i < lista.length; i++){
+    let esine = document.createElement('div');
+    let otsikko = document.createElement('h1');
+    let kuva =  document.createElement('img');
+    let kuvaus = document.createElement('p');
+    let linkki = document.createElement('p');
+    let genre = document.createElement('h3');
+
+
+    //nimi
+    let x =lista[i].show.name;
+    //linkki
+    let y =lista[i].show.url;
+    //summary
+    let z =lista[i].show.summary;
+   //kuva
+     let v = lista[i].show.image.medium;
+    //genre
+    let g =lista[i].show.genres;
+
+    try {
+       v = lista[i].show.image.medium;
+    }catch (error) {
+      console.error(error);
+      kuva.src = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
+    }
+
+
+
+      otsikko.innerHTML = x;
+      linkki.innerHTML = y;
+      kuvaus.innerHTML = z;
+      kuva.src = v;
+      genre.innerHTML = 'Genre:' +g;
+
+
+    tulosta.appendChild(esine);
+    tulosta.appendChild(otsikko);
+    tulosta.appendChild(kuva);
+    tulosta.appendChild(kuvaus);
+    tulosta.appendChild(genre);
+    tulosta.appendChild(linkki);
+
+
+
+  }
+};
+
 
 let arvo = document.querySelector("#hakuteksti");
 
 
 
 console.log('ready');
-
-
-const p = document.querySelector("#tulosta");
-
-  fetch(' http://api.tvmaze.com/search/shows?q='+haku).then(function(vastaus) {
-  }).then((resp)=>resp.json())
-  .then(function(json) {
-    let tulos = json.q;
-    return tulos.map(function(nayta) {
-      const nimi = tulos[0].name;
-      const tyyppi = tulos[0].type;
-    })
-  })
-
-  .catch(function(error) {
-    console.log(error);
-  });
+const tulosta = document.querySelector('#tulosta');
 
 
 
 // https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
 // nayta(json);
+
+//let tulos = json.q;
+//     return tulos.map(function(nayta) {
+//       const nimi = tulos[0].name;
+//       const tyyppi = tulos[0].type;
+//     })
+//
+//console.log(nimi);
+// console.log(tyyppi);
+//((resp)=>resp.json())
+//let v =lista[i].show.image.medium;
+//esine.innerHTML = v;
+
+// if (lista[i].show.image.value=null){
+//
+//       otsikko.innerHTML = x;
+//       linkki.innerHTML = y;
+//       kuvaus.innerHTML = z;
+//
+//       }else { }
